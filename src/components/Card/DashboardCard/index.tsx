a3e07c66bc34d039067d7dc25c3539cb26ce8996
Nikhil Thakur <nikhilthakur@samplemail.com>
2023-01-18 07:54:50 +0530

    Added dashboard card component

--- src/components/Card/DashboardCard/index.tsx
+++ src/components/Card/DashboardCard/index.tsx
@@ -9,21 +9,15 @@ interface DashboardProps {
 function DashboardCard({ title, description, imgUrl, postedOn }: DashboardProps) {
   return (
     <div className={styles.card}>
-      <div className={styles.mainContainer}>
-        <img
-          style={{ width: '80px', height: '80px', borderRadius: '5px' }}
-          src={imgUrl}
-          alt="companyLogo"
-        />
+      <div className={styles.main_container}>
+        <img className={styles.img} src={imgUrl} alt="companyLogo" />
         <div className={styles.container}>
-          <p style={{ fontSize: '30px', fontFamily: 'Inter' }}>{title}</p>
-          <p style={{ fontFamily: 'Inter' }}>{description}</p>
+          <p className={styles.title}>{title}</p>
+          <p>{description}</p>
         </div>
       </div>
       <div className={styles.separator} />
-      <p style={{ position: 'relative', top: '30%', textAlign: 'right', color: '#CFD2CF' }}>
-        {postedOn} min ago
-      </p>
+      <p className={styles.time_stamp}>{postedOn} min ago</p>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     react(),
     ViteWebfontDownload([
       'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
-    ]),
-  ],
+    ]),
+  ],
 })
