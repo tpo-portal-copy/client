3f0a260aef8cc77c4e25a6097eedde7d4284ccfe
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-01-18 12:57:34 +0530

    top navbar added

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import { Header } from '../components'
 import { Dashboard, Profile } from '../pages'
 import styles from './App.module.scss'
 
 function App() {
   return (
     <div className={styles.app}>
+      <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,11 @@
+import styles from './Sidebar.module.scss'
+
 function Sidebar() {
-  return <div>Sidebar</div>
+  return (
+    <div className={styles.sidebar}>
+      <h1>Sidebar</h1>
+    </div>
+  )
 }
 
 export default Sidebar
