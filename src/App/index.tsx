7a8fb814a1f42b6671978bd1ca192024adf6a56e
Nikhil Thakur <nikhilthakur@samplemail.com>
2023-02-03 18:32:28 +0530

    Done with commit

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -79,7 +79,7 @@ function App() {
           path="/profile"
           element={
             <SidebarLayout>
-            <Profile />
+              <Profile />
             </SidebarLayout>
           }
         />
