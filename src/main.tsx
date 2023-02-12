fa1499c1bbe7ecd9efc1b608766e4d2fd62b53e1
Nikhil Thakur <nikhilthakur@samplemail.com>
2023-02-12 16:59:57 +0530

    Commit after changes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './index.scss'
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
