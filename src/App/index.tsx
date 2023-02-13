7c29290ddba2f4c38141372277144e52b6d57fd2
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-02-13 15:44:08 +0530

    WIP

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import { Header } from '../components'
-import SidebarLayout from '../components/SidebarLayout'
 import {
   Dashboard,
   Profile,
@@ -14,76 +13,25 @@ import {
   Page404,
   ExperienceDetails,
 } from '../pages'
-import styles from './App.module.scss'
 
 function App() {
   return (
-    <div className={styles.app}>
+    <>
       <Header />
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/student-details-form" element={<StudentDetailsForm />} />
-        <Route
-          path="/"
-          element={
-            <SidebarLayout>
-              <Dashboard />
-            </SidebarLayout>
-          }
-        />
-        <Route
-          path="/drives"
-          element={
-            <SidebarLayout>
-              <Drives />
-            </SidebarLayout>
-          }
-        />
-        <Route
-          path="/experiences"
-          element={
-            <SidebarLayout>
-              <Experiences />
-            </SidebarLayout>
-          }
-        />
-        <Route
-          path="/experience-details/:id"
-          element={
-            <SidebarLayout>
-              <ExperienceDetails />
-            </SidebarLayout>
-          }
-        />
-
-        <Route
-          path="/statistics"
-          element={
-            <SidebarLayout>
-              <Statistics />
-            </SidebarLayout>
-          }
-        />
-        <Route
-          path="/resources"
-          element={
-            <SidebarLayout>
-              <Resources />
-            </SidebarLayout>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <SidebarLayout>
-              <Profile />
-            </SidebarLayout>
-          }
-        />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/drives" element={<Drives />} />
+        <Route path="/experiences" element={<Experiences />} />
+        <Route path="/experience-details/:id" element={<ExperienceDetails />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/404" element={<Page404 />} />
       </Routes>
-    </div>
+    </>
   )
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import { useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom'
 import styles from './Header.module.scss'
 import Sidebar from '../Sidebar'
 import useOnOutsideClick from '../../hooks/useOnOutsideClick'
+import navItems from '../../utils/Data/sidebarData'
 
 function Header() {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false)
@@ -21,16 +23,27 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <div className={styles.hamburger} onClick={openSidebar}>
-        <FontAwesomeIcon icon={faBars} className={styles.icon} />
-      </div>
-      <div className={`${styles.overlay} ${isSidebarVisible ? styles.show : ''}`}>
-        <div className={`${styles.sidebar_container}`} ref={sidebarRef}>
-          <Sidebar onLinkClickHandler={closeSidebar} isMobile />
-        </div>
-      </div>
       <div className={styles.content}>
-        <div className={styles.logo}>Sakha Nith</div>
+        <div className={`${styles.overlay} ${isSidebarVisible ? styles.show : ''}`}>
+          <div className={`${styles.sidebar_container}`} ref={sidebarRef}>
+            <Sidebar onLinkClickHandler={closeSidebar} isMobile />
+          </div>
+        </div>
+        <div className={styles.title_container}>
+          <div className={styles.hamburger} onClick={openSidebar}>
+            <FontAwesomeIcon icon={faBars} className={styles.icon} />
+          </div>
+          <div className={styles.logo}>NITH</div>
+        </div>
+        <nav className={styles.nav_items}>
+          {navItems.map((navItem) => {
+            return (
+              <NavLink key={navItem.id} to={navItem.url}>
+                {navItem.name}
+              </NavLink>
+            )
+          })}
+        </nav>
       </div>
     </header>
   )
