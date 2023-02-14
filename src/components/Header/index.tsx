3f3cd82a11fcfb0af35ee457f842bc0629c7d660
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-02-14 15:55:24 +0530

    fixes done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
 import { useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import {
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  useDisclosure,
+} from '@chakra-ui/react'
 import styles from './Header.module.scss'
 import Sidebar from '../Sidebar'
 import useOnOutsideClick from '../../hooks/useOnOutsideClick'
@@ -10,6 +18,7 @@ import navItems from '../../utils/Data/sidebarData'
 function Header() {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false)
   const sidebarRef = useRef(null)
+  const location = useLocation()
 
   const openSidebar = () => {
     setIsSidebarVisible(true)
@@ -20,6 +29,7 @@ function Header() {
   }
 
   useOnOutsideClick(sidebarRef, closeSidebar)
+  const { onOpen, onClose, isOpen } = useDisclosure()
 
   return (
     <header className={styles.header}>
@@ -33,17 +43,72 @@ function Header() {
           <div className={styles.hamburger} onClick={openSidebar}>
             <FontAwesomeIcon icon={faBars} className={styles.icon} />
           </div>
-          <div className={styles.logo}>NITH</div>
+          <Link to="/" className={styles.logo}>
+            <img src="/nithLogo.png" alt="Nith Logo" />
+            <p>NITH</p>
+          </Link>
         </div>
         <nav className={styles.nav_items}>
-          {navItems.map((navItem) => {
+          {navItems.slice(0, -1).map((navItem) => {
             return (
-              <NavLink key={navItem.id} to={navItem.url}>
-                {navItem.name}
-              </NavLink>
+              <div key={navItem.id} className={styles.nav_item}>
+                <NavLink
+                  to={navItem.url}
+                  className={`${styles.nav_item} ${
+                    location.pathname === navItem.url ? styles.selected : ''
+                  }`}
+                >
+                  {navItem.name}
+                </NavLink>
+              </div>
             )
           })}
         </nav>
+        <Popover isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
+          <PopoverTrigger>
+            <div className={styles.profile_img}>
+              <span>JK</span>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent
+            w="170px"
+            _focus={{
+              outline: 'none',
+              border: '0px',
+            }}
+            _focusVisible={{
+              outline: 'none',
+              border: '0px',
+              boxShadow: 'none',
+            }}
+          >
+            <PopoverArrow />
+            <PopoverBody
+              display="flex"
+              flexDirection="column"
+              _focus={{
+                outline: 'none',
+                border: '0px',
+                boxShadow: 'none',
+              }}
+              _focusVisible={{
+                outline: 'none',
+                border: '0px',
+                boxShadow: 'none',
+              }}
+            >
+              <Link to="/profile" className={styles.option} onClick={onClose}>
+                My Profile
+              </Link>
+              <Link to="/placement-policy" className={styles.option} onClick={onClose}>
+                Placement Policy
+              </Link>
+              <Link to="/login" className={styles.option} onClick={onClose}>
+                Logout
+              </Link>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </div>
     </header>
   )
