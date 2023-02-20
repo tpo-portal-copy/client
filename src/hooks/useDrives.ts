6dc95cd67a49282026bae0d1aeafcdd0e4b1cbf4
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-02-20 14:56:26 +0530

    useResponsive hook added

--- src/hooks/useDrives.ts
+++ src/hooks/useDrives.ts
@@ -1,5 +1,11 @@
 import { useQuery } from 'react-query'
+import { drivesAPI } from '../utils/apis'
+
+const getDrives = async () => {
+  const response = await drivesAPI.get('/')
+  return response.data
+}
 
 export default function useDrives() {
-  return useQuery('drives')
+  return useQuery('drives', getDrives)
 }
