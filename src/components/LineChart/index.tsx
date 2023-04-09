89821d17ef2b990e53fd967ebc659392ce35c714
Nikhil Thakur <nikhilthakur@samplemail.com>
2023-04-09 21:59:56 +0530

    Added tpo dashboard page

---
+++ src/components/LineChart/index.tsx
@@ -0,0 +1,100 @@
+/* eslint-disable prefer-template */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-boolean-value */
+import { ResponsiveBar } from '@nivo/bar'
+
+export default function LineChart({ data }) {
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <ResponsiveBar
+        data={data}
+        keys={['B.Tech', 'B.Arch', 'Dual Degree', 'M.Tech', 'M.Arch', 'M.Sc.', 'MBA']}
+        indexBy="month"
+        groupMode="grouped"
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        colors={{ scheme: 'nivo' }}
+        defs={[
+          {
+            id: 'dots',
+            type: 'patternDots',
+            background: 'inherit',
+            color: '#38bcb2',
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: 'lines',
+            type: 'patternLines',
+            background: 'inherit',
+            color: '#eed312',
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+        ]}
+        borderColor={{
+          from: 'color',
+          modifiers: [['darker', 1.6]],
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'country',
+          legendPosition: 'middle',
+          legendOffset: 32,
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'food',
+          legendPosition: 'middle',
+          legendOffset: -40,
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{
+          from: 'color',
+          modifiers: [['darker', 1.6]],
+        }}
+        legends={[
+          {
+            dataFrom: 'keys',
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 120,
+            translateY: 0,
+            itemsSpacing: 2,
+            itemWidth: 100,
+            itemHeight: 20,
+            itemDirection: 'left-to-right',
+            itemOpacity: 0.85,
+            symbolSize: 20,
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+        role="application"
+        ariaLabel="Nivo bar chart demo"
+        barAriaLabel={function (e) {
+          return e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue
+        }}
+      />
+    </div>
+  )
+}
