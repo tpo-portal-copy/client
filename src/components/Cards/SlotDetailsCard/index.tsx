5dfaed3afe08fef1caa12e181831012b8ea1055f
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-02-21 21:50:36 +0530

    add: styling to slots page

--- src/components/Cards/SlotDetailsCard/index.tsx
+++ src/components/Cards/SlotDetailsCard/index.tsx
@@ -7,12 +7,14 @@ function SlotCard({ slot, onBookSlot }: { slot: Slot; onBookSlot: (slot: Slot) =
     <div className={styles['slot-card']}>
       <div className={styles['profile-pic']}>
         <img src="profile-pic-url.jpg" alt="User Profile" className={styles['profile-pic-img']} />
+        <h3>{slot.title}</h3>
       </div>
       <div className={styles['slot-details']}>
-        <h3>{slot.title}</h3>
         <p>Description: {slot.description}</p>
         <p>Start Time: {slot.startTime}</p>
         <p>End Time: {slot.endTime}</p>
+      </div>
+      <div className={styles.center}>
         <button onClick={() => onBookSlot(slot)} className={styles['book-button']}>
           Book Slot
         </button>
